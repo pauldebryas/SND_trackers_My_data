f304c63bd0301b328acc46cdb5548d0dc6029269
Paul de Bryas <paul.debryas@epfl.ch>
2021-03-01 17:27:28 +0100

    Add some comments, clean up a bit the code

--- Preprocessing_script.py
+++ Preprocessing_script.py
@@ -1,4 +1,8 @@
-# --------------------------------------------------------------IMPORT AND OPTIONS FOR PREPROCESSING ----------------------------------------------------------------------
+#Preprocessing_script.py
+
+# This file is use to transform the raw data file (root file) to processed data (pickle file), in which only the needed informations are stored.
+
+# -----------------------------------IMPORT AND OPTIONS FOR PREPROCESSING-------------------------------------------------------------
 # Import Class from utils.py & net.py file
 from utils import DataPreprocess, Parameters
 from net import digitize_signal
@@ -16,16 +20,13 @@ from IPython import display
 import os
 import argparse
 from ROOT import TH1F, TFile
-# Turn off interactive plotting: for long run it makes screwing up everything
-plt.ioff()
 
-# ----------------------------------------------------  Reading the tt_cleared.pkl & y_cleared.pkl files by chunk -------------------------------------
-
+# -----------------------------------Reading the pickle files by chunk ---------------------------------------------------------------
 def reading_pkl_files(n_steps,processed_file_path):
     chunklist_TT_df = []  # list of the TT_df file of each chunk
     chunklist_y_full = [] # list of the y_full file of each chunk
 
-    # It is reading and analysing data by chunk instead of all at the time (memory leak problem)
+    # It is reading and analysing data by chunk instead of all at the same time (memory leak problem)
     #First 2 
     outpath = processed_file_path + "/{}".format(0)
     chunklist_TT_df.append(pd.read_pickle(os.path.join(outpath, "tt_cleared.pkl")))
@@ -50,60 +51,59 @@ def reading_pkl_files(n_steps,processed_file_path):
 
     return reindex_TT_df, reindex_y_full
 
-# ---------------------------------------------------- Paramaters -----------------------------------------------------------------------------------------------------
+# ----------------------------------------------------Paramaters----------------------------------------------------------------------
+# parameter of the geometry configuration
+params = Parameters("SNDatLHC") 
 
-params = Parameters("SNDatLHC")
-
-# Path to the raw Data root file and the pickle file
+# Path to the raw Data root file and the localisation of where to store the pickle file
 filename = "$HOME/Desktop/data/heavy_data/nue_CCDIS_0to200kevents.root"
 loc_of_pkl_file = "$HOME/Desktop/data/processed_data/CCDIS"
 processed_file_path = os.path.expandvars(loc_of_pkl_file)
 
 # Usualy, Data file is too large to be read in one time, that the reason why we read it by "chunk" of step_size events
-step_size = 1000    # number of event in a chunk
-file_size = 200000  # number of events to be analyse. Maximum number for DS5.root is 200'000
-
-n_steps = int(file_size / step_size) # number of chunks
-nb_of_plane = len(params.snd_params[params.configuration]["TT_POSITIONS"])
+# number of event in a chunk
+step_size = 1000
+# number of events to be analyse. Warning: Maximum number should not exceed the maximum number of events in the root file it must be a multiple of step_size
+file_size = 200000
+#file number to begging with. Useful when the raw data file is split in multiple file. Default value must be 0.
 begining_file = 0
 
+#index of the event you want to plot the signal
+index=91 # Warning: this should not exceed the maximum number of events proceed. 
+# Be careful, there is less events proceed than event in the root file (events with less than 5 hits are not proceed)
+
+# ----------------------------------------------------Global variables----------------------------------------------------------------
+n_steps = int(file_size / step_size) 
+nb_of_plane = len(params.snd_params[params.configuration]["TT_POSITIONS"])
 parser = argparse.ArgumentParser()
-parser.add_argument("step", help="As the code is a bit long, it is better to run it by step, to make sure everything works fine on the fly. First, run python Preprocessing_script.py step1, then Preprocessing_script.py step2, etc until stepX")
+parser.add_argument("step", help="First, run \"python Preprocessing_script.py Root2Pickle\". Then, if you want to display an event, run \"python Preprocessing_script.py Event_display\" ")
 args = parser.parse_args()
 
-# ----------------------------------------- PRODUCE THE tt_cleared.pkl & y_cleared.pkl IN ship_tt_processed_data/ FOLDER -------------------------------------------------
-if(args.step=="step1"):
-    print("Step1: producing tt_cleared.pkl & y_cleared.pkl file in " + loc_of_pkl_file)
+# -----------------------------------------Produce the pickle files ------------------------------------------------------------------
+if(args.step=="Root2Pickle"):
+    print("Producing tt_cleared.pkl & y_cleared.pkl file by chunk in " + loc_of_pkl_file)
 
     # It is reading and analysing data by chunk instead of all at the time (memory leak problem)
-    print("\nProducing the tt_cleared.pkl & y_cleared.pkl files by chunk")
     data_preprocessor = DataPreprocess(params)
-    os.system("mkdir -p {}".format(processed_file_path)) # create a directory ship_tt_processed_data where to store the data 
+    # create a directory where to store the data 
+    os.system("mkdir -p {}".format(processed_file_path)) 
     for i in tqdm(range(0,n_steps)):  # tqdm: make your loops show a progress bar in terminal
         raw_chunk = data_preprocessor.open_shower_file(filename, start=i*step_size, stop=(i+1)*step_size) # opening data by chunk 
         outpath = processed_file_path + "/{}".format(i+begining_file)
-        os.system("mkdir -p {}".format(outpath)) # create a directory named 'i+begining_file' where to store the data files of the 100 chunk
-        data_preprocessor.clean_data_and_save(raw_chunk, outpath) # create the tt_cleared.pkl and y_cleared.pkl file in each of the directory
+        os.system("mkdir -p {}".format(outpath)) # create a directory where to store the pickle files of the ieme chunks
+        data_preprocessor.clean_data_and_save(raw_chunk, outpath) # create the tt_cleared.pkl and y_cleared.pkl file in the directory
 
-# ------------------------------------------ LOAD THE reindex_TT_df & reindex_y_full PD.DATAFRAME --------------------------------------------------------------------------
-if(args.step=="step2"):
-    
-    print("Step2: Display an event of each planes")
-    
+# ------------------------------------------Use this to display an event--------------------------------------------------------------
+if(args.step=="Event_display"):
+    print("Display an event:")
+
     reindex_TT_df, reindex_y_full = reading_pkl_files(n_steps,processed_file_path)
 
-    #----------------------------------------- Ploting figure of the 6 component of TT_df --------------------------------------------------------------------------------------
-
-    #index of the event you want to plot the signal
-    index=91
-
     response = digitize_signal(reindex_TT_df.iloc[index], params=params, filters=nb_of_plane)
-    print("Response shape:",response.shape) # gives (6,150,185) for resolution =700 and (6,525,645) for resolution =200 and (6, 75, 93) for resolution= 1400 // (6, 724, 865), res 1050 
+    #response.shape is an important parameter: it is the input size of our CNN
+    print("Response shape:",response.shape) 
     plt.figure(figsize=(18,nb_of_plane))
     for i in range(nb_of_plane):
         plt.subplot(1,nb_of_plane,i+1)
         plt.imshow(response[i].astype("uint8") * 255, cmap='gray')
     plt.show()
-
-
-
