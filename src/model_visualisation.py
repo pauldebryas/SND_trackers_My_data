108f60fb2c7b8b8036b5389a105ec6760d4b8069
Fedor Sergeev <Theodor.Sergeev@yandex.ru>
2021-06-07 17:57:10 +0200

    Improved bayesian, general fixes

--- src/model_visualisation.py
+++ src/model_visualisation.py
@@ -51,15 +51,15 @@ def plot_2d_energy_hist(X_arr, y_true, y_pred, model_name):
     hist = ax.hist2d(X_arr[:, 0], y_true[:, 0], 
                      bins=100, norm=mpl.colors.LogNorm(), vmax=150)
     
-    plt.xlabel('pixel sum')
-    plt.ylabel('normalised energy')
+    plt.xlabel('Total number of hits in the detector')
+    plt.ylabel(r'Normalised $E_{true}$')
 
     #plt.axvline(x=min_clip, c='m', alpha=0.9, label='Min clip ' + str(min_clip))
 
     ax.plot(X_arr, y_pred, 'deeppink', marker='.', linestyle='None', alpha=0.3, label=model_name)
 
     cbar = fig.colorbar(hist[3], ax=ax)
-    cbar.set_label('# of particles')
+    cbar.set_label('Number of events')
 
     plt.legend(loc='lower right')
                 
@@ -75,15 +75,15 @@ def plot_2d_energy_hist_clip(X_full, y_true, y_pred, clip, model_name):
     hist = ax.hist2d(X_full[:, 0], y_true[:, 0], 
                      bins=100, norm=mpl.colors.LogNorm(), vmax=150)
     
-    plt.xlabel('pixel sum')
-    plt.ylabel('normalised energy')
+    plt.xlabel('Total number of hits in the detector')
+    plt.ylabel(r'Normalised $E_{true}$')
 
     plt.axvline(x=clip, c='m', alpha=0.9, label='Min clip ' + str(clip))
 
     ax.plot(X_full, y_pred, 'deeppink', marker='.', linestyle='None', alpha=0.3, label=model_name)
 
     cbar = fig.colorbar(hist[3], ax=ax)
-    cbar.set_label('# of particles')
+    cbar.set_label('Number of events')
 
     plt.legend(loc='lower right')
     return fig
@@ -134,7 +134,7 @@ def plot_res_hist(y_true, y_pred, hist_range=None, bins=100, density=False):
     n, bins, patches = ax.hist(resolution, bins, hist_range, density=density)
     
     plt.xlabel(r'$(E_{reco} - E_{true})~/~E_{true}$')
-    plt.ylabel('# particles')
+    plt.ylabel('Number of events')
 
     return fig
     
@@ -159,7 +159,7 @@ def plot_res_hist_fit(y_true, y_pred, hist_range=None, bins=100):
     plt.plot(x, stats.norm.pdf(x, loc, scale), label=label_str)
     
     plt.xlabel(r'$(E_{reco} - E_{true})~/~E_{true}$')
-    plt.ylabel('# particles')
+    plt.ylabel('Number of events')
 
     plt.legend()
     return fig
